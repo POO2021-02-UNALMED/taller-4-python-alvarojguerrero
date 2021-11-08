b70349c60d7b01a28b8173eec5451ec075825c8a
alvarojguerrero <guerreroalvarojavier@gmail.com>
2021-11-08 16:28:27 -0500

    Update grupo.py

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -10,28 +10,18 @@ class Grupo:
         self.listadoAlumnos = estudiantes
 
     def listadoAsignaturas(self, **kwargs):
-        if self._asignaturas is None:
-            self._asignaturas = []
-            for x in kwargs.values():
-                self._asignaturas.append(Asignatura(x))
+        for x in kwargs.values():
+            self._asignaturas.append(Asignatura(x))
 
-    def agregarAlumno(self, alumno, lista=[]):
+    def agregarAlumno(self, alumno, lista=None):
         if(lista is None):
+            self.listadoAlumnos = [alumno]
+        else:
             lista.append(alumno)
             self.listadoAlumnos = self.listadoAlumnos + lista
-        else:
-            self.listadoAlumnos = [alumno]
-
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 10"):
-        cls.grado = nombre
 
     @ classmethod
     def asignarNombre(cls, nombre="Grado 6"):
-        cls.grado = nombre
-
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 4"):
         cls.grado = nombre
 
     def __str__(self):
